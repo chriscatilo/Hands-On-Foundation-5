b73398ae57f7c112d2750c0b8c2dc3e306cb325b
Christopher Catilo <chriscatilo@hotmail.com>
2016-07-24 18:43:28 +0100

    Build the Slider

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,12 @@
 $(document).foundation()
+
+// slick initilisation
+$(document).ready(function(){
+    $('.my-cool-slider').slick({ 
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 6000, 
+        speed: 400
+    });
+});
+    
